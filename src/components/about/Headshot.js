ceb7bd3fd6d90cfcada52c425f5f10f288683b0c
Emily Barwinczak <ebarw@MacBook-Air.lan>
2022-03-10 11:47:05 -0500

    final updates

--- src/components/about/Headshot.js
+++
@@ -1,9 +0,0 @@
-import Emily from "./Emily.jpg"
-
-export default function Headshot() {
-    return (
-        <div>
-            <img className="headshot" src={Emily} alt="Emily"></img>
-        </div>
-    )
-}
--- src/components/skills/Skills.js
+++
@@ -1,9 +0,0 @@
-import Logos from './Logos'
-
-export default function Skills() {
-    return(
-        <div class="logoSection" id="skills">
-            <Logos />
-        </div>
-    )
-}
